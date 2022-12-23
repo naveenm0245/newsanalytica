92eb99c8ab3802cd65dffd07a99d45f1eb2910d3
Naveen Meena <naveen@Naveens-MacBook-Pro.local>
2022-12-23 18:03:30 +0530

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ function page() {
       <h1 className='flex-1 bg-red'>Naveen Meena</h1>
       <h2 className='text-red-400'>Time is {time.toString()}</h2>
       <h2 className='text-slate-600'>Naveen Meena</h2>
+      <h3 className='text-green-800 text-4xl'> How are you visiter ??</h3>
     </div>
   )
 }
